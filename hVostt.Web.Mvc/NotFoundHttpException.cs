3feaec29777be4af6e707ccd0eb9dd4dc8963719
Aleksey Gallyamov <ag@hvostt.ru>
2013-04-09 17:30:20 +0600

    Added NotFoundHttpException

---
+++ hVostt.Web.Mvc/NotFoundHttpException.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace hVostt.Web.Mvc
+{
+	public class NotFoundHttpException : HttpException
+	{
+		public NotFoundHttpException()
+			: base(404, null)
+		{
+		}
+
+		public NotFoundHttpException(string message)
+			: base(404, message)
+		{
+		}
+	}
+}
