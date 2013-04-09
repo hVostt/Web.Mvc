6e506d3e8450d0032771352d991896f5e0b91fac
Aleksey Gallyamov <ag@hvostt.ru>
2013-04-09 16:57:06 +0600

    Added StringExtensions static class
    Happy String Extensions!
    No more UGLY String.Format, only "your string format".F(params)

---
+++ hVostt.Web.Mvc/Extensions/StringExtensions.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace hVostt.Web.Mvc.Extensions
+{
+
+	/// <summary>
+	/// Happy String Extensions!
+	/// No more UGLY String.Format, only "your string".F(params)
+	/// </summary>
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Replaces one or more format items in a specified string with the string representation
+		/// of a specified object.
+		/// </summary>
+		/// <param name="format">A composite format string (as String.Format).</param>
+		/// <param name="arg0">The object to format.</param>
+		/// <returns>
+		/// A copy of format in which any format items are replaced by the string 
+		/// representation of <paramref name="arg0"/>.
+		/// </returns>
+		public static string F(this string format, object arg0)
+		{
+			return String.Format(format, arg0);
+		}
+
+		/// <summary>
+		/// Replaces the format item in a specified string with the string representation
+		/// of a corresponding object in a specified array.
+		/// </summary>
+		/// <param name="format">A composite format string (as String.Format).</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		/// <returns>
+		/// A copy of format in which the format items have been replaced by the string 
+		/// representation of the corresponding objects in <paramref name="args"/>.
+		/// </returns>
+		public static string F(this string format, params object[] args)
+		{
+			return String.Format(format, args);
+		}
+
+		/// <summary>
+		/// Replaces the format item in a specified string with the string representation
+		/// of a corresponding object in a specified array. A specified parameter supplies
+		/// culture-specific formatting information.
+		/// </summary>
+		/// <param name="format">A composite format string (as String.Format).</param>
+		/// <param name="provider">An object that supplies culture-specific formatting information.</param>
+		/// <param name="args">An object array that contains zero or more objects to format.</param>
+		/// <returns>
+		/// A copy of format in which the format items have been replaced by the string
+		/// representation of the corresponding objects in <paramref name="args"/>.
+		/// </returns>
+		public static string F(this string format, IFormatProvider provider, params object[] args)
+		{
+			return String.Format(provider, format, args);
+		}
+
+		/// <summary>
+		/// Replaces the format items in a specified string with the string representation
+		/// of two specified objects.
+		/// </summary>
+		/// <param name="format">A composite format string (as String.Format).</param>
+		/// <param name="arg0">The first object to format.</param>
+		/// <param name="arg1">The second object to format.</param>
+		/// <returns>
+		/// A copy of format in which format items are replaced by the string representations
+		/// of <paramref name="arg0"/> and <paramref name="arg1"/>.
+		/// </returns>
+		public static string F(this string format, object arg0, object arg1)
+		{
+			return String.Format(format, arg0, arg1);
+		}
+
+		/// <summary>
+		/// Replaces the format items in a specified string with the string representation
+		/// of three specified objects.
+		/// </summary>
+		/// <param name="format">A composite format string (as String.Format).</param>
+		/// <param name="arg0">The first object to format.</param>
+		/// <param name="arg1">The second object to format.</param>
+		/// <param name="arg2">The third object to format.</param>
+		/// <returns>
+		/// A copy of format in which the format items have been replaced by the string
+		/// representations of <paramref name="arg0"/>, <paramref name="arg1"/>, and <paramref name="arg2"/>.
+		/// </returns>
+		public static string F(this string format, object arg0, object arg1, object arg2)
+		{
+			return String.Format(format, arg0, arg1, arg2);
+		}
+	}
+}
