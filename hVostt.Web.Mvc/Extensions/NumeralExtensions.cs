72751b2af040665717da3a23aadc4758b3010fc9
Aleksey Gallyamov <ag@hvostt.ru>
2013-05-08 02:46:40 +0600

    Changes in NumeralExtensions
    Add ToRawString for Decimal and Int32 (it's custom)
    Add "zero" parameter in ToPrice and ToPriceString — default value for d
    = 0

--- hVostt.Web.Mvc/Extensions/NumeralExtensions.cs
+++ hVostt.Web.Mvc/Extensions/NumeralExtensions.cs
@@ -6,24 +6,36 @@ namespace hVostt.Web.Mvc.Extensions
 {
 	public static class NumeralExtensions
 	{
-		public static IHtmlString ToPrice(this decimal d)
+		public static string ToPriceString(this decimal d, string zero = "")
 		{
-			return new HtmlString(ToPriceString(d));
+			return d == 0
+				? zero
+				: d.ToString(d - Decimal.Truncate(d) > 0 ? "#,#.00" : "#,#");
 		}
 
-		public static string ToPriceString(this decimal d)
+		public static IHtmlString ToPrice(this decimal d, string zero = "")
 		{
-			return d.ToString(d - Decimal.Truncate(d) > 0 ? "#,#.00" : "#,#");
+			return new HtmlString(ToPriceString(d, zero));
+		}
+
+		public static string ToRawString(this decimal d)
+		{
+			return d.ToString(CultureInfo.InvariantCulture);
+		}
+
+		public static string ToRawString(this int i)
+		{
+			return i.ToString(CultureInfo.InvariantCulture);
 		}
 
 		public static IHtmlString ToRaw(this decimal d)
 		{
-			return new HtmlString(d.ToString(CultureInfo.InvariantCulture));
+			return new HtmlString(ToRawString(d));
 		}
 
 		public static IHtmlString ToRaw(this int i)
 		{
-			return new HtmlString(i.ToString(CultureInfo.InvariantCulture));
+			return new HtmlString(ToRawString(i));
 		}
 	}
 }
